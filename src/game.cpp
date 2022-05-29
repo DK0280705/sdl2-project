76a5819c7f2340eb941445bd70f1f62fed67e612
DK0280705 <deka.ganteng028@gmail.com>
2022-05-29 11:26:03 +0700

    Okay enough

--- src/game.cpp
+++ src/game.cpp
@@ -19,9 +19,15 @@ void Game::update(float delta)
 {
     static bool reverse = false;
     static float r = 0;
-    r += (reverse ? -100.0f : 100.0f) * delta;
-    if (r >= 255.0f) reverse = true;
-    else if (r <= 0.0f) reverse = false;
+    r += (reverse ? -80.0f : 80.0f) * delta;
+    if (r >= 255.0f) { 
+        r = 255.0f;
+        reverse = true;
+    }
+    else if (r <= 0.0f) {
+        r = 0.0f;
+        reverse = false;
+    }
     SDL_SetRenderDrawColor(renderer, 76, (int)r, 0, 255);
     SDL_RenderClear(renderer); 
     SDL_RenderPresent(renderer);
